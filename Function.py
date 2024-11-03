e31e6ceda52333ec65e68fd4f26b3865ec8ae277
Vipinkale <vipin111kale@gmail.com>
2024-11-03 12:06:26 +0530

    Add files via upload

---
+++ Function.py
@@ -0,0 +1,27 @@
+#Addition
+def add_numbers(a, b):
+    return a + b
+
+result = add_numbers(3, 5)
+print(result) 
+
+#Substraction
+def sub_number(a,b):
+    return a-b
+
+result = sub_number(10,5)
+print(result)
+
+# Multiplication
+def mul_number (a,b):
+    return a*b
+
+result = mul_number(5,5)
+print(result)
+
+# Division
+def Div_number (a,b):
+    return a/b
+
+result = Div_number(100,10)
+print(result)
---
+++ Function2.py
@@ -0,0 +1,23 @@
+def get_name_and_age():
+    name = "Alice"
+    age = 30
+    return name, age
+
+name, age = get_name_and_age()
+print(name, age)  
+
+def get_name_and_age():
+    name = "Prerna"
+    age = 22
+    return name, age
+
+name, age = get_name_and_age()
+print(name, age)  
+
+def get_name_and_age():
+    name = "Adi"
+    age = 21
+    return name, age
+
+name, age = get_name_and_age()
+print(name, age)  
